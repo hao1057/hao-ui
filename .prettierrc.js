85a4e4b58261a20ee75694cef099cbc6a791cd8f
hao3960 <396094224@qq.com>
2022-06-21 00:19:16 +0800

    second commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false, //末尾逗号， default： true
+  singleQuote: true, //单引号， default： false
+  trailingComma: "none", // 末尾分号 default：es5 
+  arrowParens: "avoid",
+  proseWrap: "never",
+  endOfLine: "auto",
+}
